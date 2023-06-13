22246bc801dd47bb3b3911bb7b736128b833c7ae
margdaly <margaret.daly@gmail.com>
2023-06-13 17:25:50 -0600

    test/feat: Add get_directions to map service

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe MapService do
+  before { @map_service = MapService.new }
+
   describe '#get_lat_lon' do
     it 'returns lat and lon for a city', :vcr do
-      map_service = MapService.new
+      expect(@map_service.get_lat_lon('denver,co')).to be_a Hash
+      expect(@map_service.get_lat_lon('denver,co')).to have_key :results
 
-      expect(map_service.get_lat_lon('denver,co')).to be_a Hash
-      expect(map_service.get_lat_lon('denver,co')).to have_key :results
-
-      results = map_service.get_lat_lon('denver,co')[:results]
+      results = @map_service.get_lat_lon('denver,co')[:results]
       expect(results).to be_an Array
       expect(results.first).to be_a Hash
       expect(results.first).to have_key :locations
@@ -22,6 +22,20 @@ RSpec.describe MapService do
       expect(lat_lng).to be_a Hash
       expect(lat_lng).to have_key :lat
       expect(lat_lng).to have_key :lng
+    end
+  end
+
+  describe '#get_directions' do
+    it 'returns details for directions', :vcr do
+      origin = 'New York, NY'
+      destination = 'Los Angeles, CA'
+      directions = @map_service.get_directions(origin, destination)
+
+      expect(directions).to be_a Hash
+      expect(directions).to have_key :route
+      expect(directions[:route]).to be_a Hash
+      expect(directions[:route]).to have_key :formattedTime
+      expect(directions[:route][:formattedTime]).to be_a String
     end
   end
 end
